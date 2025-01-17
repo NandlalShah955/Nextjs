9212318f80014f2db51cfa772aa75d6dc2767911
NandlalShah <shahnandlal49@gmail.com>
2025-01-17 15:25:29 +0530

    basic nextjs project added

--- next-js-practice/src/components/Header.jsx
+++ next-js-practice/src/components/Header.jsx
@@ -1,9 +1,31 @@
+import Link from 'next/link';
 import React from 'react'
-
+import '../app/main.css'
+// import { useRouter } from 'next/router'
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    return (
+        <header>
+            <div>logo</div>
+            <ul>
+                <li>
+                    <Link href='/'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/about'>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/course'>
+                        Course
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    )
 }
 
 export default Header;
